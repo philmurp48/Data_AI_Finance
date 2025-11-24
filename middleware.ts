1200a5eff5b81e9bef1c6a958610dc3372505104
mikearonaccenture <mikearonaccenture@users.noreply.github.com>
2025-11-24 13:30:05 -0500

    Fix: Exclude logo.png and images from middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -27,7 +27,9 @@ export const config = {
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
          * - public (public files)
+         * - logo.png (logo file)
+         * - images (image directory)
          */
-        '/((?!api|_next/static|_next/image|favicon.ico|public).*)',
+        '/((?!api|_next/static|_next/image|favicon.ico|public|logo.png|images).*)',
     ],
 }; 
